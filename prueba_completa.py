c42bb3b133c6ae966c8561c269da5ca3ca7265ae
vanessa9711 <37115514+vanessa9711@users.noreply.github.com>
2019-06-18 10:56:06 -0500

    Add files via upload

---
+++ prueba_completa.py
@@ -0,0 +1,12 @@
+import lexer_rules
+import parser_rules
+
+from ply.lex import lex
+from ply.yacc import yacc
+
+text = "(14 + 6) / 2-(5)"
+lexer = lex(module=lexer_rules)
+parser = yacc(module=parser_rules)
+expression = parser.parse(text, lexer)
+print (expression)
+
